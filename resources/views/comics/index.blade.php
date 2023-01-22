d1b33a3ee95dddbe3de825550365a1495039b804
alessandro-cordoglio <alessandro.cord20@gmail.com>
2023-01-22 23:51:36 +0100

    aggiunto button che reindirizza alla show

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -26,6 +26,7 @@
                   <td> {{$comic->series}} </td>
                   <td> {{$comic->sale_date}} </td>
                   <td> {{$comic->type}} </td>
+                  <td><a href="{{route('comics.show', $comic->id)}}" class="btn btn-primary">Espandi</a></td>
                 </tr>
                 @endforeach
             </tbody>
